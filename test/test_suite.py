bb1c2c75dcb32720afc020513ea2653741602a4a
Niels Nes <niels@cwi.nl>
2026-02-02 13:34:47 +0100

    fix visit DOUBLE_PRECISION for issue #57, thanks to semigodking
    enabled test as we allow limit with queries ..

--- test/test_suite.py
+++ test/test_suite.py
@@ -1,18 +1,6 @@
 import pytest
 from sqlalchemy.testing.suite import *
 
-# Failures
-class FetchLimitOffsetTest(FetchLimitOffsetTest):
-    @pytest.mark.skip(reason="This test does some LIMIT statements in "
-                      "the middle of the query, this is not supported.")
-    def test_limit_render_multiple_times(*args, **kwargs):
-        """
-        This test does some LIMIT statements in the middle of the query,
-        this is not supported.
-        """
-        pass
-
-
 # Errors
 @pytest.mark.skip(reason="The tests of this class use self-reference "
                   "foreign keys which are NOT supported by MonetDB")
