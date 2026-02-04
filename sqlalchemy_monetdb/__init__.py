2b1b3c71c8db07e5aa49381667308a39e0a00f47
Joeri van Ruth <jvr@pixie.blub.net>
2026-02-04 14:17:20 +0100

    The authoritative version number is now in sqlalchemy_monetdb/__init__.py

--- sqlalchemy_monetdb/__init__.py
+++ sqlalchemy_monetdb/__init__.py
@@ -2,9 +2,4 @@
 SQLAlchemy backend for MonetDB
 """
 
-import pkg_resources
-
-try:
-    __version__ = pkg_resources.require("sqlalchemy-monetdb")[0].version
-except pkg_resources.DistributionNotFound:
-    __version__ = "dev"
+__version__ = '2.1.0a0'
